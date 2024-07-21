0058a8c07a143d7d074e95a963f10471f06cabee
Emily Vu <emilyvu@vt.edu>
2024-07-21 15:00:01 -0400

    change to my name

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import { Fragment } from 'react';
 import Head from 'next/head';
+import { Fragment } from 'react';
 
 import FeaturedPosts from '../components/home-page/featured-posts';
 import Hero from '../components/home-page/hero';
@@ -9,7 +9,7 @@ function HomePage(props) {
   return (
     <Fragment>
       <Head>
-        <title>Uncle John V.' Blog</title>
+        <title>Uncle Emily Vu's Blog</title>
         <meta
           name='description'
           content='I post about programming and web development.'
